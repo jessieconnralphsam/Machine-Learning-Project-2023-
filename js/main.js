580fd53724ae168f69bd818f8b7abf730d724dd7
jessieconnralph <jessieconnralph.official@gmail.com>
2023-06-03 19:19:46 +0800

    revert

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 //access camera
-navigator.mediaDevices.getUserMedia({ video: true })
-    .then(function (stream){
-        var videoElement = document.getElementById('videoElement');
-        videoElement.srcObject = stream;
-    })
-    .catch(function (error) {
-        console.log('Error accessing camera:', error);
-    })
+// navigator.mediaDevices.getUserMedia({ video: true })
+//     .then(function (stream){
+//         var videoElement = document.getElementById('videoElement');
+//         videoElement.srcObject = stream;
+//     })
+//     .catch(function (error) {
+//         console.log('Error accessing camera:', error);
+//     })
