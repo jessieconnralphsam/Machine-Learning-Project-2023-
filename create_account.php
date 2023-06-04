4a357144b595f1ec66c1690a26c02a6016770243
jessieconnralph <jessieconnralph.official@gmail.com>
2023-06-04 13:36:52 +0800

    create acc delete <i>

--- create_account.php
+++ create_account.php
@@ -16,7 +16,7 @@
     <div class="form-group">
         <label for="password">Password</label>
         <input type="password" name="password" id="password" class="form-control" value="" autocomplete="off">
-        <small><i>Leave this blank if you dont want to change the password.</i></small>
+        <!-- <small><i>Leave this blank if you dont want to change the password.</i></small> -->
     </div>
     <div class="form-group">
         <label for="" class="control-label">Avatar</label>
