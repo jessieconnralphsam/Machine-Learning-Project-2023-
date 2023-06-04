59416cc23ee0c36841833eb0893f950da7d763bc
jessieconnralph <jessieconnralph.official@gmail.com>
2023-06-04 12:48:50 +0800

    new changes

--- index.php
+++ index.php
@@ -1,42 +1,88 @@
-
-<?php
-    //db connection
-    require_once 'connection.php';
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Leaf Disease | Detection</title>
-    <link rel="stylesheet" href="style.css?v=2">
-    <script src="js/main.js"></script>
-    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-</head>
-<body>
+<?php require_once('config.php'); ?>
+ <!DOCTYPE html>
+<html lang="en" class="" style="height: auto;">
+<?php require_once('inc/header.php') ?>
+  <body class="layout-top-nav">
     <div class="wrapper">
-        <div class="title"><i class="fas fa-leaf"></i> Leaf Disease Detection</div>
-        <div class="form">
-            <div class="bot-inbox inbox">
-                <div class="icon">
-                    <i class="fas fa-robot"></i>
-                </div>
-                <div class="msg-header">
-                    <p>Hello there, upload <br> image below</p>
-                </div>
-            </div>
-        </div>
-        <div class="typing-field">
-            <div class="input-data">
-                <input id="data" type="file" accept="image/*">
-                <!-- <video id="videoElement" autoplay></video> -->
-                <!--camera here -->
-                <button id="send-btn">check</button>
-            </div>
-        </div>
-    </div> 
+     <?php require_once('inc/topBarNav.php') ?>
+              
+     <?php $page = isset($_GET['page']) ? $_GET['page'] : 'home';  ?>
+      <!-- Content Wrapper. Contains page content -->
+      <div class="content-wrapper pt-3" style="min-height: 567.854px;">
+     
+        <!-- Main content -->
+        <section class="content  text-dark">
+          <div class="container">
+            <?php 
+              if(!file_exists($page.".php") && !is_dir($page)){
+                  include '404.html';
+              }else{
+                if(is_dir($page))
+                  include $page.'/index.php';
+                else
+                  include $page.'.php';
 
-    <script src="index.js"></script>
-</body>
+              }
+            ?>
+          </div>
+        </section>
+        <!-- /.content -->
+  
+  <div class="modal fade" id="uni_modal" role='dialog'>
+    <div class="modal-dialog modal-md modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+        <h5 class="modal-title"></h5>
+      </div>
+      <div class="modal-body">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" id='submit' onclick="$('#uni_modal form').submit()">Save</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+      </div>
+      </div>
+    </div>
+  </div>
+  <div class="modal fade" id="confirm_modal" role='dialog' data-backdrop='static'>
+    <div class="modal-dialog modal-md modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+        <h5 class="modal-title">Confirmation</h5>
+      </div>
+      <div class="modal-body">
+        <div id="delete_content"></div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" id='confirm' onclick="">Continue</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+      </div>
+    </div>
+  </div>
+  <div class="modal fade" id="uni_modal_right" role='dialog'>
+    <div class="modal-dialog modal-full-height  modal-md" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+        <h5 class="modal-title"></h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span class="fa fa-arrow-right"></span>
+        </button>
+      </div>
+      <div class="modal-body">
+      </div>
+      </div>
+    </div>
+  </div>
+  <div class="modal fade" id="viewer_modal" role='dialog'>
+    <div class="modal-dialog modal-md" role="document">
+      <div class="modal-content">
+              <button type="button" class="btn-close" data-dismiss="modal"><span class="fa fa-times"></span></button>
+              <img src="" alt="">
+      </div>
+    </div>
+  </div>
+      </div>
+      <!-- /.content-wrapper -->
+      <?php require_once('inc/footer.php') ?>
+  </body>
 </html>
--- js/main.js
+++
@@ -1,9 +0,0 @@
-//access camera
-// navigator.mediaDevices.getUserMedia({ video: true })
-//     .then(function (stream){
-//         var videoElement = document.getElementById('videoElement');
-//         videoElement.srcObject = stream;
-//     })
-//     .catch(function (error) {
-//         console.log('Error accessing camera:', error);
-//     })
